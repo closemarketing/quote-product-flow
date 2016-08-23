3389bc0f68a6a827bc90ccc3eae1594838367ebf
Sumit Mallik <mit2sumit@yahoo.com>
2016-08-23 22:56:33 +0545

    slight changes

--- pbc.php
+++ pbc.php
@@ -856,7 +856,7 @@ class PBCPlugin
 				$html2pdf->writeHTML($content);
 				$html2pdf->Output(WPPBC_PLUGIN_DIR."/pdf/$filename.pdf", 'F');
 				//$html2pdf->close();
-				$return = array('type'=>'success', 'msg'=>WPPBC_PLUGIN_URL."/pdf/$filename.pdf");
+				$return = array('type'=>'success', 'msg'=>WPPBC_PLUGIN_URL."pdf/$filename.pdf");
 			} catch (Html2PdfException $e) {
 				$formatter = new ExceptionFormatter($e);
 				$return = array('type'=>'error', 'msg'=>"Unexpected Error!<br>Can't load PDF this time!<br>".$formatter->getHtmlMessage());
