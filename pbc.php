42cbd18fdf096c891d8a10ba9aeef6d4c2dc439e
davidperezgar <david@closemarketing.es>
2025-01-23 13:24:21 +0100

    quantity

--- pbc.php
+++ pbc.php
@@ -47,6 +47,7 @@ require_once WPPBC_PLUGIN_PATH . 'vendor/autoload.php';
 
 // Helpers.
 require_once WPPBC_PLUGIN_PATH . 'includes/helpers/class-calculations.php';
+require_once WPPBC_PLUGIN_PATH . 'includes/helpers/class-show-parts.php';
 require_once WPPBC_PLUGIN_PATH . 'includes/helpers/class-show-template-wizard.php';
 
 // Include files.
