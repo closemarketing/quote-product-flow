604008ffdde30725e398667a0a99435124af2c99
Sumit Kumar Mallik <mit2sumit@yahoo.com>
2025-07-26 09:46:50 +0545

    Show prices per user role #46

--- includes/helpers/class-show-parts.php
+++ includes/helpers/class-show-parts.php
@@ -154,7 +154,9 @@ class SHOW {
 			<h2 class="title"><?php esc_html_e( 'Actual Configuration', 'pbc' ); ?></h2>
 			<table>
 				<?php
-				$show_prices = get_option( 'pbc_budget_show_prices' );
+                $user = wp_get_current_user();
+				$show_prices = get_option( 'pbc_show_prices_user_' . $user->roles[0] );
+				$show_prices = ! empty( $show_prices ) ? 'yes' : 'no';
 				if ( 'calculate' === $cstep ) {
 					$count       = count( $phases );
 					$total_price = 0;
