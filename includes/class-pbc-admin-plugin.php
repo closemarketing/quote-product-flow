b5c57f542d5e4674838e703f42079c63954aa303
castellon <alexcastellon077@gmail.com>
2025-12-02 10:04:50 +0100

    Fix linting

--- includes/class-pbc-admin-plugin.php
+++ includes/class-pbc-admin-plugin.php
@@ -427,9 +427,9 @@ class PBC_Admin_Plugin {
 						if ( ! empty( $result ) ) {
 							$activate_results = json_decode( $result, true );
 
-						// Check if activation was successful OR if it says "already activated with this instance".
-						$is_success        = ! empty( $activate_results ) && true === $activate_results['success'] && true === $activate_results['activated'];
-						$is_already_active = ! empty( $activate_results ) &&
+							// Check if activation was successful OR if it says "already activated with this instance".
+							$is_success        = ! empty( $activate_results ) && true === $activate_results['success'] && true === $activate_results['activated'];
+							$is_already_active = ! empty( $activate_results ) &&
 												isset( $activate_results['code'] ) &&
 												'100' === $activate_results['code'] &&
 												isset( $activate_results['error'] ) &&
